77db284d89014e4f59f2864892f88c6ffaa80cde
pelluritulasiprasad <126033500+pelluritulasiprasad@users.noreply.github.com>
2023-02-22 21:27:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: "MTA3Nzk4MTkzOTk5NjI5NTIyMQ.GhvrVM.sdQF-6D_23yB_86i6Maqk4cGtZHoApKrQOUC7A",
-    bot_invite_link: "YOUR BOT INVITE LINK",
+    bot_invite_link: "https://discord.com/api/oauth2/authorize?client_id=1077981939996295221&permissions=8&scope=bot",
     url: "https://join4join.xyz",
     footer: "discord.gg/join4join",
     color: "#e84e4b",
